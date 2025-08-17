3c7e6f678a392ec2718290c2dee600169ecf6634
Cinnamon <213962814+irunonironic@users.noreply.github.com>
2025-08-17 21:42:31 +0530

    done

--- src/Components/Mains.jsx
+++ src/Components/Mains.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './NavBar/NavBar'
-import Page1 from './page1'
+import Page1 from './Page1'
 import Page2 from './Page2'
 import Page3 from './Page3'
 import Page4 from './Page4'
